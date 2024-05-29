435ebc556a1241fe1a82f835c945883989629495
Icewelle <corentin.luby@gmail.com>
2024-05-29 10:48:18 +0200

    fini ?

--- client.c
+++ client.c
@@ -3,22 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icetea <icetea@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cluby <cluby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 22:32:05 by icetea            #+#    #+#             */
-/*   Updated: 2024/05/27 23:58:13 by icetea           ###   ########.fr       */
+/*   Created: 2024/05/27 22:32:05 by cluby             #+#    #+#             */
+/*   Updated: 2024/05/28 20:15:44 by cluby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "libft/includes/libft.h"
 
+ void send_signal(int pid, unsigned char character)
+{
+	int    i;
+	unsigned char temp_char;
+
+	i = 8;
+	temp_char = character;
+	while (i > 0)
+	{
+		i--;
+		temp_char = character >> i;
+		if (temp_char % 2 == 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(42);
+	}
+}
 int	main(int argc, char *argv[])
 {
+	int		i;
+	__pid_t	pid;
+	char	*str;
+
 	if (argc != 3)
 		return (write(2, "Wrong number of arguments\n", 26), 1);
-	(void)argv;
-	printf("Client\n");
+	i = 0;
+	pid = ft_atoi(argv[1]);
+	str = argv[2];
+	while (str[i])
+	{
+		send_signal(pid, str[i]);
+		i++;
+	}
+	send_signal(pid, END_TRANSMISSION);
 	return (0);
 }
--- server.c
+++ server.c
@@ -3,19 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icetea <icetea@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cluby <cluby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 22:32:02 by icetea            #+#    #+#             */
-/*   Updated: 2024/05/27 23:48:09 by icetea           ###   ########.fr       */
+/*   Created: 2024/05/27 22:32:02 by cluby             #+#    #+#             */
+/*   Updated: 2024/05/28 21:10:41 by cluby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
+#include "libft/includes/libft.h"
+
+void handle_signal(int signal)
+{
+	static unsigned char current_char;
+	static int    bit_index;
+
+	current_char |= (signal == SIGUSR1);
+	bit_index++;
+	if (bit_index == 8)
+	{
+		if (current_char == END_TRANSMISSION)
+			ft_printf("\n");
+		else
+			ft_printf("%c", current_char);
+		bit_index = 0;
+		current_char = 0;
+	}
+	else
+		current_char <<= 1;
+	
+}
 
 int	main()
 {
-	printf("server\n");
+	ft_printf("%d\n", getpid());
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
+	while (1)
+		pause();
 	return (0);
 }
